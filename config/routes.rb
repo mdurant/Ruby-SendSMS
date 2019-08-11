7263aa0078f93d76eccc507f513aa9fa8954f739
MAURICIO DURAN T <mauriciodurant@gmail.com>
2019-08-10 23:23:10 -0400

    Se configuran las rutas para el proyecto , y ademas se agrega gemfile de dotenv para uso de credenciales

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'mesages/new'
-
-  get 'mesages/create'
-
+  root 'mesages#new'
+  post '/mesages', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
